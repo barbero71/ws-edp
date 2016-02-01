fa16a6ac51931210bfa3806d03342139fe623d0f
mbarberini <mbarberini@rhel01.schultze.locale>
2016-02-01 15:59:20 +0100

    Configured to run in a Tomcat Server

--- src/main/java/it/sogesispa/dev/ws/edp/WsEdpApplication.java
+++ src/main/java/it/sogesispa/dev/ws/edp/WsEdpApplication.java
@@ -3,6 +3,7 @@ package it.sogesispa.dev.ws.edp;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class WsEdpApplication {
 
